ed2f43f5eff29021de34d67deabfbe952c1d382f
zhaogong <zhaogong@local>
2026-07-16 17:40:06 +0800

    fix(selfie): current is string, revert wrong .url access

--- app/pages/play/selfie.vue
+++ app/pages/play/selfie.vue
@@ -44,8 +44,8 @@
 
     <div v-else-if="current">
       <PuzzleGame
-        :key="current.url"
-        :image-url="current.url"
+        :key="current"
+        :image-url="current"
         :piece-count="pieceCount"
         mode-label="📷 自拍上传"
         :show-score="false"
